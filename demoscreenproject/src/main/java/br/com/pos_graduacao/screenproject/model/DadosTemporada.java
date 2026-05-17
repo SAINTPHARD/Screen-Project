b5811da5a3a7427f3cb92c786cb10cb47ac75e76
Robedson SAINTPHARD <pierreobed67@gmail.com>
2026-05-17 16:15:56 -0300

    feat: implementa record DadosTemporada para mapear lista de episódios por temporada

--- demoscreenproject/src/main/java/br/com/pos_graduacao/screenproject/model/DadosTemporada.java
+++ demoscreenproject/src/main/java/br/com/pos_graduacao/screenproject/model/DadosTemporada.java
@@ -1,4 +1,20 @@
 package br.com.pos_graduacao.screenproject.model;
 
-public class DadosTemporada {
-}
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import java.util.List;
+
+/**
+ * Classe de modelo para representar os dados de uma temporada.
+ * Responsável por : encapsular os atributos e comportamentos relacionados aos dados de uma temporada, como número da temporada, lista de episódios, etc.
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DadosTemporada(
+	/**
+	 *  O número da temporada é mapeado a partir do campo "Season" no JSON, e a lista de episódios 
+	 *  é mapeada a partir do campo "Episodes". A anotação @JsonAlias é usada para garantir que os 
+	 *  campos sejam corretamente mapeados mesmo que os nomes no JSON sejam diferentes dos nomes dos atributos na classe
+	 */
+    @JsonAlias("Season") Integer numero, 
+    @JsonAlias("Episodes") List<DadosEpisodio> episodios
+) {}
